ad27d38b29116d92b73505c778d110c19cebf0a0
anvarknian <anvarknian@yandex.ru>
2020-05-11 06:37:14 +0300

    new

--- src/main/scala/ru/vtb/kafka/streams/utils/ProcessFunction.scala
+++ src/main/scala/ru/vtb/kafka/streams/utils/ProcessFunction.scala
@@ -1,5 +1,10 @@
 package ru.vtb.kafka.streams.utils
 
+import java.sql.Connection
+import java.time.{Instant, ZoneId, ZonedDateTime}
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+
 import oracle.jdbc.pool.OracleDataSource
 import org.slf4j.LoggerFactory
 
@@ -14,47 +19,50 @@ class ProcessFunction(configuration: Configuration, customerID: String) {
   private val DRIVER = configuration.dbConfig.driver
   private val USER = configuration.dbConfig.user
   private val PASSWORD = configuration.dbConfig.password
-  private val tables = configuration.dbConfig.tables
+  private val TABLES = configuration.dbConfig.tables
+
+  logger.info(s"Setting connection to $URL ...")
+
   Class.forName(DRIVER)
   private val connection = new OracleDataSource()
   connection.setURL(URL)
   connection.setUser(USER)
   connection.setPassword(PASSWORD)
+  val con: Connection = connection.getConnection()
+  if (con.isValid(1)) {
+    logger.info(s"Successfully Connected to $URL")
+  }
 
   def queryDatabase(table: String): List[Map[String, String]] = {
 
     var fullTable = new ListBuffer[Map[String, String]]()
 
-    Try(connection.getConnection()) match {
-      case Success(con) =>
-        logger.info("Successfully Connected")
-        val statement = con.createStatement()
-        Try(statement
-          .executeQuery(s"select * from $table WHERE $customerID")) match {
-
-          case Success(query) =>
-            val md = query.getMetaData
-            val columnCount = md.getColumnCount
-            var columnNames = new ListBuffer[String]()
-            for (i <- 1 to columnCount) {
-              columnNames += md.getColumnName(i)
-            }
-            while (query.next()) {
-              var data = new ListBuffer[String]()
-              for (i <- 1 to columnCount) {
-                data += query.getString(i)
-              }
-              val row: Map[String, String] = (columnNames zip data) (breakOut)
-              fullTable += row
-            }
-          case Failure(e) =>
-            logger.error("Error while querying database: ", e)
+    val statement = con.createStatement()
+    val start = Instant.now()
+    Try(statement.executeQuery(s"select * from $table WHERE $customerID")) match {
+      case Success(query) =>
+        logger.info(s"Successfully sent query to table '$table'")
+        val md = query.getMetaData
+        val columnCount = md.getColumnCount
+        var columnNames = new ListBuffer[String]()
+        for (i <- 1 to columnCount) {
+          columnNames += md.getColumnName(i)
         }
-
-        con.close()
-      case Failure(e) => logger.error("Error while connecting to database: ", e)
+        while (query.next()) {
+          var data = new ListBuffer[String]()
+          for (i <- 1 to columnCount) {
+            data += query.getString(i)
+          }
+          val row: Map[String, String] = (columnNames zip data) (breakOut)
+          fullTable += row
+        }
+      case Failure(exception) => logger.error(s"Error while querying database '$table': ", exception)
     }
+    val end = Instant.now()
+    val diffInSecs = ChronoUnit.SECONDS.between(start, end)
+    logger.info(s"Querying table '$table' took: $diffInSecs")
     fullTable.toList
+
   }
 
   def toJson(query: Any): String = query match {
@@ -67,7 +75,11 @@ class ProcessFunction(configuration: Configuration, customerID: String) {
   }
 
   def run(): String = {
-    val allTables: Map[String, List[Map[String, String]]] = tables.map(table => table -> queryDatabase(table)).toMap
+    val allTables: Map[String, List[Map[String, String]]] = TABLES.map(table => table -> queryDatabase(table)).toMap
+    con.close()
+    if (con.isClosed()) {
+      logger.info(s"Successfully closed Connection to $URL")
+    }
     toJson(allTables)
   }
 }
